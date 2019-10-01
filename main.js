967d6315d87ac240b3a1e64cafb18931bb7eebb9
Jakub Kowalczuk <jak.kowalczuk@gmail.com>
2019-10-01 21:43:38 +0200

    Dom

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ console.log(name);
 console.log(age);
 
 console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const header = document.querySelector ('.mainHeader')
+console.log(header.innerHTML)
+
+header.innerHTML = 'Siema JS!'
+
+const paragraphs = document.querySelectorAll ('p')
+console.log(paragraphs)
+console.log(paragraphs[0])
