38fd1aa2bff70ef70b9bb993817ea9321d356031
Jakub Kowalczuk <jak.kowalczuk@gmail.com>
2019-10-03 21:37:09 +0200

    feat: JSlogic

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 console.log('Siemanko');
 const name='Jakub';
-const age=31;
+const age=75;
 console.log(name);
 console.log(age);
 
 console.log(`Nazywam się ${name} i mam ${age} lat`);
 
-const header = document.querySelector ('.mainHeader')
-console.log(header.innerHTML)
+const header = document.querySelector ('.main__header');
+console.log(header.innerHTML);
 
 header.innerHTML = 'Siema JS!'
 
-const paragraphs = document.querySelectorAll ('p')
-console.log(paragraphs)
-console.log(paragraphs[0])
+const about = document.querySelector ('.about__paragraph--js');
+about.innerHTML = `Nazywam się ${name} i mam ${age} lat`;
+
+if ('javascript' != 'java') {
+    console.log('to prawda!')
+}
+
+
+if (age < 20) {
+    console.log('masz mniej niz 20 lat');
+} else if ((age >= 20) && (age <= 30)) {
+    console.log('masz wiecej niz 20 lat ale mniej niz 30');
+} else {
+    console.log('jestes 31+');
+}
+
+switch (age) {
+    case 20:
+        console.log('masz równo 20 lat');
+    break;
+    case 30:
+        console.log('masz równo 30 lat');
+    break;
+    default:
+        console.log(`masz ${age} lat`);
+    break;
+}
+
+const amIOld = (age > 70) ? 'yes' : 'no';
+console.log(amIOld);
